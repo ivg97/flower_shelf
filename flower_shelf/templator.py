a8009ccb75b4425c7b7e6a09b5efb0db99056739
ivg97 <gavr97.97@mail.ru>
2022-04-04 17:51:04 +0300

    lesson 3

--- flower_shelf/templator.py
+++ flower_shelf/templator.py
@@ -1,5 +1,6 @@
-from jinja2 import Template
-import os
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+
 
 
 def render(template_name, folder='templates', **kwargs):
@@ -10,8 +11,8 @@ def render(template_name, folder='templates', **kwargs):
     return: html шаблон
 
     '''
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding='utf-8') as file:
-        template = Template(file.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
 
+    template = env.get_template(template_name)
     return template.render(**kwargs)
